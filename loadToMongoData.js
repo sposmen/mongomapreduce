648ff627f37c2fad0e05b6d5344d9d92f7a9024c
Jaime Giraldo <sposmen@gmail.com>
2015-11-14 16:26:25 -0500

    Working throttler

--- loadToMongoData.js
+++ loadToMongoData.js
@@ -11,6 +11,7 @@ process.on("uncaughtException", function (err) {
 
 var path = require('path'),
   fs = require('graceful-fs'),
+  Throttler = require('throttler'),
   Lazy = require('lazy'),
   MongoWritableStream = require('mongo-writable-stream');
 
@@ -23,16 +24,16 @@ var movieInsertStream = new MongoWritableStream({
   collection: 'movies'
 });
 
-var moviesStream = fs.createReadStream(path.join(__dirname, 'data', 'movie_titles.txt'));
+var moviesStream = fs.createReadStream(path.join(__dirname, 'data','movie_titles.txt'));
 
 new Lazy(moviesStream).lines.forEach(function (row) {
   var rowData = row.toString().split(',');
   movieInsertStream.write({
-    id: rowData[0],
-    year: rowData[1],
-    title: rowData[2]
+    id:rowData[0],
+    year:rowData[1],
+    title:rowData[2]
   });
-}).on('pipe', function () {
+}).on('pipe', function(){
   movieInsertStream.end();
 });
 
@@ -42,46 +43,48 @@ var getTitle = function (movId) {
   return 'mv_' + (new Array(7 - movId.toString().length + 1).join('0')) + movId + '.txt';
 };
 
-var processMovie = function (movId, cb) {
-  var title = getTitle(movId);
+var processMovie = function (movId) {
+  throttler.add(function(cb) {
+    var title = getTitle(movId);
 
-  var movieRankInsertStream = new MongoWritableStream({
-    url: url,
-    collection: 'ratings'
-  }).on('finish', function(){
-    cb('MovieID:' + movId + ' ended');
-  });
+    var movieRankInsertStream = new MongoWritableStream({
+      url: url,
+      collection: 'ratings'
+    }).on('finish', function(){
+      cb('MovieID:' + movId + ' ended');
+    });
 
+    var movieRankStream = fs.createReadStream(path.join(__dirname, 'data', 'training_set', title));
 
-  var movieRankStream = fs.createReadStream(path.join(__dirname, 'data', 'training_set', title));
+    new Lazy(movieRankStream).lines.map(String).skip(1).forEach(function (row) {
 
-  new Lazy(movieRankStream).lines.map(String).skip(1).forEach(function (row) {
+      var rowData = row.split(',');
 
-    var rowData = row.split(','),
-      rowObject = {
+      movieRankInsertStream.write({
         movieId: movId,
         customerId: rowData[0],
         rating: parseInt(rowData[1]),
         date: new Date(rowData[2])
-      };
-
-    movieRankInsertStream.write(rowObject);
-
-  }).on('pipe', function () {
-    movieRankInsertStream.end();
+      });
+    }).on('pipe', function () {
+      movieRankInsertStream.end();
+    });
   });
 };
 
-var singleThrottler = function (movId) {
-  if (movId <= 17770) {
-    console.log("Starting with ", movId, ' file.');
-    processMovie(movId, function (text) {
-      console.log(text);
-      singleThrottler(movId++);
-    })
-  } else {
-    console.log("Finished")
-  }
-};
+var throttler = new Throttler({
+  'maxActive' : 4,
+  'wait' : 2500
+});
 
-singleThrottler(1);
+throttler.on('job-started', function(stats) {
+  console.log('Job started. Current stats: %j', stats);
+}).on('job-completed', function(stats) {
+  console.log('Job completed. Current stats: %j', stats);
+}).on('complete', function(stats) {
+  console.log('All jobs complete. Final stats: %j', stats);
+});
+
+for (var movId = 1; movId <= 17770; movId++) {
+  processMovie(movId);
+}
